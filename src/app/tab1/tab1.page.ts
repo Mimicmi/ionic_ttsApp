dffa1db79a113e9c7c8de64f20c8ea8d82b43f26
Simon Frezard <simon.frezard@gmail.com>
2021-02-03 16:49:39 +0100

    used storage, multiple button for clipboard and clearing it, using native tts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+import { Clipboard } from '@ionic-native/clipboard/ngx';
+
+import { Storage } from '@ionic/storage';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,12 +11,51 @@ import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  locale = "fr-FR";
+  rate = 2;
 
-  constructor(private tts: TextToSpeech) { }
+  constructor(private tts: TextToSpeech,
+    private clipboard: Clipboard,
+    private storage: Storage
+  ) { }
+
+  text2Speech: string;
 
   speak() {
-    this.tts.speak('Hello World')
+    this.tts.speak({
+      text: this.text2Speech,
+      locale: this.locale,
+      rate: this.rate
+    })
       .then(() => console.log('Success'))
       .catch((reason: any) => console.log('TTS ERROR', reason));
+  }
+
+  paste() {
+    this.clipboard.paste().then(
+      (text: string) => {
+        this.text2Speech = this.text2Speech ? this.text2Speech + '\n' + text : text;
+      },
+      (reject: string) => {
+        console.log('Clipboard Error: ' + reject);
+      }
+    );
+  }
+  clear() {
+    this.text2Speech = "";
+  }
+  stopSpeaking() {
+    this.tts.speak('').then(() => console.log("Success")).catch((reason: any) => console.log(reason));
+  }
+
+  ionViewWillEnter() {
+    this.storage.get('locale').then((val) => {
+      console.log('Récup des réglages locale', val);
+      this.locale = val ?? val;
+    });
+    this.storage.get('rate').then((val) => {
+      console.log('Récup du rate', val);
+      this.rate = val ?? val;
+    });
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  locale = "fr-FR";
+  rate = 2;
 
+  constructor(private storage: Storage) { }
+
+  saveLocale(event) {
+    this.storage.set('locale', this.locale);
+  }
+
+  saveRate(event) {
+    this.storage.set('rate', this.rate);
+  }
+
+  ionViewWillEnter() {
+    console.log('Récup des données');
+    this.storage.get('locale').then((val) => {
+      console.log('Récup des réglages locale', val);
+      this.locale = val ?? val;
+    });
+    this.storage.get('rate').then((val) => {
+      console.log('Récup du rate', val);
+      this.rate = val ?? val;
+    });
+  }
 }
